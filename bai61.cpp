4064c66a6bb0b859b5bc8b2382089731d22b1f62
Your Name <you@example.com>
2023-02-22 15:59:56 +0700

    cau truc du lieu cay bai 61

---
+++ bai61.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+
+using namespace std;
+
+struct node{
+	int data;
+	node *left;
+	node *right;
+};
+
+node *insert(node *t, int x){
+	if(t == NULL){
+		node *tmp = new node;
+		tmp->data = x;
+		tmp->left = NULL;
+		tmp->right = NULL;
+		return tmp;
+	} else {
+		if( x < t->data){
+			t->left = insert(t->left, x);
+		} else {
+			t->right = insert(t->right, x);
+		}
+	}
+}
+
+void printTree(node *t){
+	if(t != NULL){
+		printTree(t->left);
+		cout<< t->data << " ";
+		printTree(t->right);
+	}
+}
+
+int main(){
+	int n,x;
+	cin >> n;
+	node *t = NULL;
+	for(int i=0; i<n; i++){
+		cin >> x;
+		t = insert(t,x);
+	}
+	printTree(t);
+}
